97ecf4f386a033b9f494558af9fc29185153c97f
Cesar Lopez <505046190@penske.com>
2025-05-16 12:54:29 -0700

    Tratar de armar parametro de costos como si fuera una tabla, probar funciones de DataFrame

--- airline_model.py
+++ airline_model.py
@@ -12,15 +12,20 @@ model.A = pyo.Set(initialize=aviones_df['Avion'])
 model.F = pyo.Set(initialize=vuelos_df['Vuelo']) 
 model.D = pyo.RangeSet(1, 150) # Pass this as a param, prompt for this value from UI
 
+f_v_1 = costos_df["Vuelo"] == "Vuelo_1"
+
+costos_df.where(f_v_1, inplace=True)
+print(costos_df.head(30))
+
 # Parametros
-# Decidir si declararlos o usarlos directamente desde los data frames
+# model.cost = 
 
 # Variables de decision
-model.x = pyo.Var(model.F, model.A, model.D, initialize=1)
-model.y = pyo.Var(model.A, model.D, initialize=1)
+# model.x = pyo.Var(model.F, model.A, model.D, initialize=1)
+# model.y = pyo.Var(model.A, model.D, initialize=1)
 
-# Funcion objetivo
-def func_objetivo(m):
-    return sum( sum( sum( m.x[f, a, d] * costos_df[f, a] for a in m.A) for f in m.F ) for d in m.D )
+# # Funcion objetivo
+# def func_objetivo(m):
+#     return sum( sum( sum( m.x[f, a, d] * 1 for a in m.A) for f in m.F ) for d in m.D )
 
-model.obj = pyo.Objective(rule=func_objetivo, sense=pyo.minimize)
+# model.obj = pyo.Objective(rule=func_objetivo, sense=pyo.minimize)

467650001314a8cdae8dd0494cbec82fcaf3ade7
Cesar Lopez <ce.lopez.dj@gmail.com>
2025-05-19 21:22:43 -0700

    Se usa pivote para cambiar costos_df

--- airline_model.py
+++ airline_model.py
@@ -12,20 +12,16 @@ model.A = pyo.Set(initialize=aviones_df['Avion'])
 model.F = pyo.Set(initialize=vuelos_df['Vuelo']) 
 model.D = pyo.RangeSet(1, 150) # Pass this as a param, prompt for this value from UI
 
-f_v_1 = costos_df["Vuelo"] == "Vuelo_1"
-
-costos_df.where(f_v_1, inplace=True)
-print(costos_df.head(30))
-
 # Parametros
-# model.cost = 
+costos_df = costos_df.pivot(index='Vuelo', columns='Avion', values='Costo_Operacion')
+print(costos_df)
 
 # Variables de decision
-# model.x = pyo.Var(model.F, model.A, model.D, initialize=1)
-# model.y = pyo.Var(model.A, model.D, initialize=1)
+model.x = pyo.Var(model.F, model.A, model.D, initialize=1)
+model.y = pyo.Var(model.A, model.D, initialize=1)
 
-# # Funcion objetivo
-# def func_objetivo(m):
-#     return sum( sum( sum( m.x[f, a, d] * 1 for a in m.A) for f in m.F ) for d in m.D )
+# Funcion objetivo
+def func_objetivo(m):
+    return sum(sum(sum(m.x[f, a, d] * costos_df.loc[f, a] for a in m.A) for f in m.F) for d in m.D)
 
-# model.obj = pyo.Objective(rule=func_objetivo, sense=pyo.minimize)
+model.obj = pyo.Objective(rule=func_objetivo, sense=pyo.minimize)
